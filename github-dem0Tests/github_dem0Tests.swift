16b3c008cddea25c2182d1de9e815973ca1b53c9
Andrea Mercy <andreamercyd@gmail.com>
2025-10-31 08:24:08 +0530

    Initial Commit

---
+++ github-dem0Tests/github_dem0Tests.swift
@@ -0,0 +1,17 @@
+//
+//  github_dem0Tests.swift
+//  github-dem0Tests
+//
+//  Created by Andrea Mercy on 31/10/25.
+//
+
+import Testing
+@testable import github_dem0
+
+struct github_dem0Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
